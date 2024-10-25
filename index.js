9774d887ca2d8f8894130428642806f6e5ccc075
feygit <shock-defiant-limb@duck.com>
2024-10-25 03:03:27 +0300

    fix swagger

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const kullaniciRouter = require("./routes/kullanici.router");
 const app = express();
 app.use(express.json());
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs, {
+  customCss:'.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
   customCssUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css",
 }));
 
